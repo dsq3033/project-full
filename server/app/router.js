ba17dbf7ba0c42feb99b5fdcd28b18f559f672de
shiqi.dong <shiqi.dong@ucarinc.com>
2020-05-12 16:03:47 +0800

    feat(main): 新增cz

--- server/app/router.js
+++ server/app/router.js
@@ -3,6 +3,7 @@
 /**
  * @param {Egg.Application} app - egg application
  */
+console.log('start');
 module.exports = app => {
   const { router, controller } = app
   router.get('/', controller.home.index)
